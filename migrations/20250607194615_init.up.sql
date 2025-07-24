b955bf0f4218b63984562ba416fea9baeef0e620
Santhosh <skumar7403@gmail.com>
2025-07-24 14:31:22 +0530

    fts5 with content table

--- migrations/20250607194615_init.up.sql
+++ migrations/20250607194615_init.up.sql
@@ -44,3 +44,27 @@ CREATE TABLE IF NOT EXISTS Comments(
     body text NOT NULL,
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+
+CREATE VIRTUAL TABLE IF NOT EXISTS Articles_fts using fts5(
+    title,
+    description,
+    body,
+    content='Articles',
+    tokenize='porter'
+);
+
+CREATE TRIGGER IF NOT EXISTS articles_ai AFTER INSERT ON articles BEGIN
+    INSERT INTO articles_fts(rowid, title, description, body) VALUES (NEW.oid, NEW.title,NEW.description, NEW.body);
+END;
+
+-- Update trigger: When a document is updated, delete old entry and insert new one into FTS5
+CREATE TRIGGER IF NOT EXISTS articles_au AFTER UPDATE ON articles BEGIN
+    INSERT INTO articles_fts(articles_fts, rowid, title, description, body) VALUES ('delete', OLD.oid, OLD.title, OLD.description, OLD.body);
+    INSERT INTO articles_fts(rowid, title, description, body) VALUES (NEW.oid, NEW.title, NEW.description, NEW.body);
+END;
+
+-- Delete trigger: When a document is deleted, remove it from FTS5
+CREATE TRIGGER IF NOT EXISTS articles_ad AFTER DELETE ON articles BEGIN
+    INSERT INTO articles_fts(articles_fts, rowid, title, description, body) VALUES ('delete', OLD.oid, OLD.title, OLD.description, OLD.body);
+END;
