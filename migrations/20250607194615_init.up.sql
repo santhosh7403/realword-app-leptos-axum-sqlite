3d37a7ccd9bf8381952d98ca933dc1ea41ab5a2c
Santhosh <skumar7403@gmail.com>
2025-07-25 10:48:33 +0530

    trigger for articles and articletags

--- migrations/20250607194615_init.up.sql
+++ migrations/20250607194615_init.up.sql
@@ -47,6 +47,7 @@ CREATE TABLE IF NOT EXISTS Comments(
 
 
 CREATE VIRTUAL TABLE IF NOT EXISTS Articles_fts using fts5(
+    slug UNINDEXED,
     title,
     description,
     body,
@@ -55,16 +56,49 @@ CREATE VIRTUAL TABLE IF NOT EXISTS Articles_fts using fts5(
 );
 
 CREATE TRIGGER IF NOT EXISTS articles_ai AFTER INSERT ON articles BEGIN
-    INSERT INTO articles_fts(rowid, title, description, body) VALUES (NEW.oid, NEW.title,NEW.description, NEW.body);
+    INSERT INTO articles_fts(rowid, slug, title, description, body) VALUES (NEW.oid, NEW.slug, NEW.title,NEW.description, NEW.body);
 END;
 
 -- Update trigger: When a document is updated, delete old entry and insert new one into FTS5
 CREATE TRIGGER IF NOT EXISTS articles_au AFTER UPDATE ON articles BEGIN
-    INSERT INTO articles_fts(articles_fts, rowid, title, description, body) VALUES ('delete', OLD.oid, OLD.title, OLD.description, OLD.body);
-    INSERT INTO articles_fts(rowid, title, description, body) VALUES (NEW.oid, NEW.title, NEW.description, NEW.body);
+    INSERT INTO articles_fts(articles_fts, rowid, slug, title, description, body) VALUES ('delete', OLD.oid, OLD.slug, OLD.title, OLD.description, OLD.body);
+    INSERT INTO articles_fts(rowid, slug, title, description, body) VALUES (NEW.oid, NEW.slug, NEW.title, NEW.description, NEW.body);
 END;
 
 -- Delete trigger: When a document is deleted, remove it from FTS5
 CREATE TRIGGER IF NOT EXISTS articles_ad AFTER DELETE ON articles BEGIN
-    INSERT INTO articles_fts(articles_fts, rowid, title, description, body) VALUES ('delete', OLD.oid, OLD.title, OLD.description, OLD.body);
+    INSERT INTO articles_fts(articles_fts, rowid, slug, title, description, body) VALUES ('delete', OLD.oid, OLD.slug, OLD.title, OLD.description, OLD.body);
 END;
+
+CREATE VIRTUAL TABLE IF NOT EXISTS articletags_fts using fts5(
+    article UNINDEXED,
+    tag,
+    content='articletags',
+    tokenize='porter'
+);
+
+CREATE TRIGGER IF NOT EXISTS articletags_ai AFTER INSERT ON articletags BEGIN
+    INSERT INTO articletags_fts(rowid, article, tag) VALUES (NEW.oid, NEW.article, NEW.tag);
+END;
+
+-- Update trigger: When a document is updated, delete old entry and insert new one into FTS5
+CREATE TRIGGER IF NOT EXISTS articletags_au AFTER UPDATE ON articletags BEGIN
+    INSERT INTO articletags_fts(articletags_fts, rowid, article, tag) VALUES ('delete', OLD.oid, OLD.article, OLD.tag);
+    INSERT INTO articletags_fts(rowid, article, tag) VALUES (NEW.oid, NEW.article, NEW.tag);
+END;
+
+-- Delete trigger: When a document is deleted, remove it from FTS5
+CREATE TRIGGER IF NOT EXISTS articletags_ad AFTER DELETE ON articles BEGIN
+    INSERT INTO articletags_fts(articletags_fts, rowid, article, tag) VALUES ('delete', OLD.oid, OLD.article, OLD.tag);
+END;
+
+
+
+
+-- sqlite> select snippet(articles_fts, 2, '==========', '===========', '....',32)   from articles_fts as af join articles a on af.rowid=a.oid where af.body match 'gene' ;
+-- sqlite> select a.slug, highlight(articles_fts, 2, '*****', '*****')   from articles_fts as af join articles a on af.rowid=a.oid where af.body match 'develop' ;
+
+
+-- sqlite> select snippet(articles_fts,0, '***','****','>>>',2), snippet(articles_fts, 1, '****', '****' , '>>>' , 3) ,snippet(articles_fts, 2, '==========', '===========', '....',2)   from articles_fts as af join articles a on af.rowid=a.oid where articles_fts match 'colonize' limit 4 ;
+-- Mars ***Colonization****>>>|Examining the challenges>>>|....==========colonizing=========== Mars....
+-- sqlite> select snippet(articles_fts,0, '***','****','>>>',2), snippet(articles_fts, 1, '****', '****' , '>>>' , 3) ,snippet(articles_fts, 2, '==========', '===========', '....',2)   from articles_fts as af join articles a on af.rowid=a.oid where articles_fts match 'colonize' limit 4 ;
